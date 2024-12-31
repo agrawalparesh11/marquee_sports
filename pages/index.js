ba5b384b45433cc3f23b4fd81f88920aadcdfffd
Aayush Agarwal <agarwalaayushindia@gmail.com>
2024-12-31 16:21:19 +0530

    sliding images working

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
 export default function Home() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true, // Start with autoplay disabled
+  };
+
   const slides = [
     "/slides/slide1.jpg",
     "/slides/slide2.jpg",
@@ -10,16 +20,19 @@ export default function Home() {
   ];
 
   return (
-    <div>
+    <div style={{ maxWidth: "800px", margin: "0 auto", textAlign: "center" }}>
       <h1>Interactive Presentation</h1>
-      {slides.map((slide, index) => (
-        <img
-          key={index}
-          src={slide}
-          alt={`Slide ${index + 1}`}
-          style={{ width: "100%", height: "400px", objectFit: "contain", marginBottom: "20px" }}
-        />
-      ))}
+      <Slider {...settings}>
+        {slides.map((slide, index) => (
+          <div key={index}>
+            <img
+              src={slide}
+              alt={`Slide ${index + 1}`}
+              style={{ width: "100%", height: "400px", objectFit: "contain" }}
+            />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
